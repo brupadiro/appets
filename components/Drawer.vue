9b3cb5d12fc32030366e1cad90dc95fddf308c3f
drolegc <drolegc@gmail.com>
2020-11-24 18:33:12 -0300

    Cambiando diseño - empezando por el home con un drawer

---
+++ components/Drawer.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+       
+</div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                drawer: false,
+                group: null
+            }
+        }
+    }
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,35 +1,130 @@
 <template>
-  <v-app>
-    <v-app-bar fixed app class="elevation-3" dense color="verde_suave">
-      <v-spacer></v-spacer>
-      <v-toolbar-title v-text="'Muro'" class="verde_fuerte--text overline" />
-      <v-spacer></v-spacer>
-      <v-btn icon small @click="addPublication = !addPublication">
-        <v-icon color="verde_fuerte" >mdi-tooltip-plus-outline</v-icon>
-      </v-btn>
-    </v-app-bar>
-    <v-content class="blue-grey lighten-5">
-      <nuxt />
-    </v-content>
-    <bottom-navigation-bar></bottom-navigation-bar>
-    <create-post v-model="addPublication"></create-post>
-  </v-app>
-</template>
+    <v-app>
+        <v-app-bar
+        color="orange"
+        class="white--text elevation-0"
+        >
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-spacer></v-spacer>
+            <v-toolbar-title>My files</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn icon>
+                <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+        </v-app-bar>
+        <v-navigation-drawer
+            v-model="drawer"
+            color="orange"
+            app
+            temporary
+            >
+      <v-list
+        nav
+        dense
+        class="pl-3 pt-3"
+      >
+      
+        <v-list-item-group
+          v-model="group"
+        >
+            <v-list-item-avatar size="70">
+            <img src="https://randomuser.me/api/portraits/women/81.jpg">
+            </v-list-item-avatar>
+            <v-list-item class="mb-5" two-line>
+    
+              <v-list-item-content>
+                <v-list-item-title class="white--text"><h2>Pascual</h2></v-list-item-title>
+                <v-list-item-subtitle class="white--text">Maldonado</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+    
+    
+          <v-list-item to="/profile">
+            <v-list-item-icon><v-icon color="white">mdi-account</v-icon></v-list-item-icon>
+            <v-list-item-title class="white--text" >Mi perfil</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon><v-icon color="white">mdi-ticket</v-icon></v-list-item-icon>
+            <v-list-item-title class="white--text" >Actividad</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon><v-icon color="white">mdi-bell</v-icon></v-list-item-icon>
+            <v-list-item-title class="white--text" >Notificaciones</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon><v-icon color="white">mdi-account-multiple</v-icon></v-list-item-icon>
+            <v-list-item-title class="white--text" >Amigos</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon><v-icon color="white">mdi-message-text</v-icon></v-list-item-icon>
+            <v-list-item-title class="white--text" >Mensajes</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon><v-icon color="white">mdi-image-multiple</v-icon></v-list-item-icon>
+            <v-list-item-title class="white--text" >Galeria</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon><v-icon color="white">mdi-cog</v-icon></v-list-item-icon>
+            <v-list-item-title class="white--text" >Configuracion</v-list-item-title>
+          </v-list-item>
+
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+      <v-content class="blue-grey lighten-5">
+        <nuxt />
+      </v-content>
+      <create-post v-model="addPublication"></create-post>
+    </v-app>
+  </template>
 
 <script>
-    import BottomNavigationBar from "~/components/BottomNavigationBar.vue"
     import createPost from "~/components/createPost.vue"
 
     export default {
         middleware: "auth",
         components: {
             createPost,
-            BottomNavigationBar
         },
         data() {
             return {
                 addPublication: false,
+                drawer: false,
+                group: null,
+                items: [{
+                    title: "Mi perfil",
+                    icon: "mdi-account"
+                }, {
+                    title: "Actividad",
+                    icon: "mdi-ticket"
+                }, {
+                    title: "Notificaciones",
+                    icon: "mdi-bell"
+                }, {
+                    title: "Aimgos",
+                    icon: "mdi-account-multiple"
+                }, {
+                    title: "Mensajes",
+                    icon: "mdi-message-text"
+                }, {
+                    title: "Galeria",
+                    icon: "mdi-image-multiple"
+                }, {
+                    title: "Configuracion",
+                    icon: "mdi-cog"
+                }]
             }
+        },
+        watch: {
+            group(val) {
+                this.drawer = false
+            },
         },
         methods: {}
     }
